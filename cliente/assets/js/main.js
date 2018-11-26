e41e3d62b297b0b973aedd5ec2ccf45da45fa153
Ricardo Rodríguez <rjrodriguezalvarez97@gmail.com>
2018-11-26 17:07:29 +0100

    Add some comments and refactor

--- cliente/assets/js/main.js
+++ cliente/assets/js/main.js
@@ -5,13 +5,112 @@ import {Templates} from './templates/Templates'
 var servicio_API = new Servicio_API('http://localhost:3000/')
 var templatesPrueba = new Templates();
 
+/**
+ * Hide or show the forum list
+ */
+function toggleForumList(){
+    var forumList = document.getElementById('divForumsList');
+    forumList.style.display = forumList.style.display === 'block' ? 'none' : 'block';
+}
+/**
+ * Return to the forum list
+ */
+function backToForumList(){
+    document.getElementById('divSubforumList').style.display= 'none';
+    toggleForumList();
+}
 
+/**
+ * Check if a user is logged
+ */
+function isLogged(){
+    return localStorage.token !== undefined;
+}
+
+/**
+ * Hide or show the login form
+ */
+function toggleLoginForm(){
+
+    var form = document.getElementById('divFormLogin');
+    isLogged() ? form.style.display = "none" : form.style.display = "block";
+}
+/**
+ * Creates and error with the given message
+ * @param {string} errorMessage 
+ */
 function createErrorLogin(errorMessage){
 
     var errorHTML = templatesPrueba.errorTemplate({message: errorMessage});
     var errorLogin = document.getElementById('errorLogin');
     errorLogin.innerHTML=errorHTML;
     errorLogin.style.display = 'block';
+}
+
+/**
+ * Creates the navbar
+ */
+function createNavbar(){
+    var userExists = {};
+    if(localStorage.token && localStorage.id){
+       userExists.user = true;
+    }
+    document.getElementById('navbar').innerHTML = templatesPrueba.navbarTemplate(userExists);
+    try{ 
+        document.getElementById('logout').addEventListener('click', function() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('id');
+        location.reload();
+    })}
+    catch{//When a user is not logged in element logout doesn't exist  
+    }
+}
+/**
+ * Creates the forum list
+ */
+function createForumList(){
+    if(isLogged()){
+        servicio_API.getForums().then(function (result){
+            var divForumList = document.getElementById('divForumsList');
+
+            divForumsList.innerHTML = templatesPrueba.forumListTemplate(result);
+            divForumsList.style.display = 'block';
+        })
+    }
+
+}
+/**
+ * Creates a list with all the subforums  that belong to the forum
+ * @param {int} id forum id
+ * @param {string} name forum name
+ */
+function createSubforumList(id,name){
+    if(isLogged()){
+        servicio_API.getSubforumsByForumId(id).then(function (result){
+            var data = {};
+            data.data = result;
+            data.name = name;
+            var divSubforumList = document.getElementById('divSubforumList');
+            divSubforumList.innerHTML = templatesPrueba.subForumListTemplate(data);
+            divSubforumList.style.display = 'block';
+            toggleForumList();
+
+        })
+    }
+}
+/**
+ * Create a forum inside of the modal
+ */
+function createForum(){
+    var createForumNameEntry = document.getElementById('createForumNameEntry');
+    var createForumDescriptrionEntry = document.getElementById('createForumDescriptrionEntry');
+    var payload = {
+        name: createForumNameEntry.value,
+        description: createForumDescriptrionEntry.value
+    };
+    servicio_API.createForum(payload).then(function (response){
+        location.reload();
+    })
 }
 
 function forumDetails(id){
@@ -26,9 +125,8 @@ function forumDetails(id){
         updateButton.idForum = id;
         console.log("id del boton: " + updateButton.idForum);
     })
-
 }
-window.forumDetails = forumDetails;
+
 
 function updateForum(event){
     var forumNameEntry = document.getElementById('forumNameEntry');
@@ -48,50 +146,7 @@ function deleteForum(id){
         location.reload();
     })
 }
-function createForum(){
-    var createForumNameEntry = document.getElementById('createForumNameEntry');
-    var createForumDescriptrionEntry = document.getElementById('createForumDescriptrionEntry');
-    var payload = {
-        name: createForumNameEntry.value,
-        description: createForumDescriptrionEntry.value
-    };
-    servicio_API.createForum(payload).then(function (response){
-        location.reload();
-    })
-}
-window.deleteForum = deleteForum;
-function createNavbar(){
-    var userExists = {};
-    if(localStorage.token && localStorage.id){
-       userExists.user = true;
-    }
-    document.getElementById('navbar').innerHTML = templatesPrueba.navbarTemplate(userExists);
-    try{ 
-        document.getElementById('logout').addEventListener('click', function() {
-        localStorage.removeItem('token');
-        localStorage.removeItem('id');
-        location.reload();
-    })}
-    catch{//When a user is not logged in element logout doesn't exist  
-    }
-}
 
-function createSubforumList(id,name){
-    if(isLogged()){
-        servicio_API.getSubforumsByForumId(id).then(function (result){
-            var data = {};
-            data.data = result;
-            data.name = name;
-            var divSubforumList = document.getElementById('divSubforumList');
-            divSubforumList.innerHTML = templatesPrueba.subForumListTemplate(data);
-            divSubforumList.style.display = 'block';
-            toggleForumList();
-
-        })
-        
-
-    }
-}
 
 function subforumDetails(id){
     servicio_API.getSubforum(id).
@@ -108,38 +163,8 @@ function deleteSubforum(id){
     })
 
 }
-window.deleteSubforum = deleteSubforum;
-window.subforumDetails = subforumDetails;
-window.createSubforumList = createSubforumList;
-function toggleForumList(){
-    var forumList = document.getElementById('divForumsList');
-    forumList.style.display = forumList.style.display === 'block' ? 'none' : 'block';
-}
-function backToForumList(){
-    document.getElementById('divSubforumList').style.display= 'none';
-    toggleForumList();
-}
-window.backToForumList = backToForumList;
-function isLogged(){
-    return localStorage.token !== undefined;
-}
 
-function createForumList(){
-    if(isLogged()){
-        servicio_API.getForums().then(function (result){
-            var divForumList = document.getElementById('divForumsList');
 
-            divForumsList.innerHTML = templatesPrueba.forumListTemplate(result);
-            divForumsList.style.display = 'block';
-        })
-    }
-
-}
-function toggleLoginForm(){
-
-    var form = document.getElementById('divFormLogin');
-    isLogged() ? form.style.display = "none" : form.style.display = "block";
-}
 document.addEventListener("DOMContentLoaded", function(){
     
     document.getElementById('formLogin').addEventListener('submit', function(e){
@@ -172,6 +197,10 @@ document.addEventListener("DOMContentLoaded", function(){
     createNavbar();
     createForumList();
 
-
-
 });
+window.backToForumList = backToForumList;
+window.deleteSubforum = deleteSubforum;
+window.subforumDetails = subforumDetails;
+window.createSubforumList = createSubforumList;
+window.forumDetails = forumDetails;
+window.deleteForum = deleteForum;
